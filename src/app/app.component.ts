39589e8998e668b7504b47c327806ccc2a957439
johan <johanmcaza@gmail.com>
2026-03-05 18:59:39 +0100

    nitial app customization and hot reload verification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ioc-angular-hobbyton-johan';
+
+  constructor(){
+    console.log("Hobbyton app successfully initialized");
+  }
 }
