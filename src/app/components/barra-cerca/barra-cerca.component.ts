8245c1f9dea27c82db024161eba22b0792ce240f
johan <johanmcaza@gmail.com>
2026-03-25 19:03:55 +0100

    feat ex4: implement search form with ngModel validation and real-time filtering

--- src/app/components/barra-cerca/barra-cerca.component.ts
+++ src/app/components/barra-cerca/barra-cerca.component.ts
@@ -1,17 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-barra-cerca',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './barra-cerca.component.html',
   styleUrl: './barra-cerca.component.scss'
 })
 export class BarraCercaComponent {
+  searchTerm: string="";
   @Output() searchEvent =new EventEmitter<string>();
 
-  onSearch(value: string){
-    this.searchEvent.emit(value);
+  onSearch(){
+    this.searchEvent.emit(this.searchTerm);
 
   }
 }
